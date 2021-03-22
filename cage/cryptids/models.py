91b09af166ace7cb9466b087f9dcbc4a93545d84
Carys <caryspritch@gmail.com>
2021-03-22 14:43:22 +0000

    added models

--- cage/cryptids/models.py
+++ cage/cryptids/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import datetime
+from django.utils import timezone
+class Animals(models.Model):
+    animals_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+    
+    def __str__(self):
+        return self.animals_text
 
-# Create your models here.
+    @property
+    def published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+class Location(models.Model):
+    Animals = models.ForeignKey(Animals, on_delete=models.CASCADE)
+    location_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.location_text
+
