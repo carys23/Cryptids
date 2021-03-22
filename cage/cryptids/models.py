b8d18e7c3620c62dda75726f1a24da271625f44f
Carys <caryspritch@gmail.com>
2021-03-22 14:14:07 +0000

    Added models

--- cage/cryptids/models.py
+++ cage/cryptids/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+class Animals(models.Model):
+   
+    def __str__(self):
+        return self.animals_text
 
-# Create your models here.
+class Location(models.Model):
+    
+    def __str__(self):
+        return self.location_text
