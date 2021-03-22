b6f2a7454b513c1e2db59498d212c14cd518daa7
Carys <caryspritch@gmail.com>
2021-03-22 15:17:52 +0000

    added config

--- cage/cage/settings.py
+++ cage/cage/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cryptids.apps.CryptidsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
