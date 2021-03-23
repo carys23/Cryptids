e809f5844844335820192bc320e15e2f62f30b71
andreigoncearenco <goncea.illest1@gmail.com>
2021-03-23 17:13:22 +0200

    views

--- cage/cryptids/urls.py
+++ cage/cryptids/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("/animal/<int:animal_id>/", views.detail, name="detail"),
-    path("/location/<int:location_id>/", views.place, name="place"),
-    path("/locations/", views.places, name="places"),
+    path("animal/<int:animal_id>/", views.detail, name="detail"),
+    path("location/<int:location_id>/", views.place, name="place"),
+    path("locations/", views.places, name="places"),
 ]
