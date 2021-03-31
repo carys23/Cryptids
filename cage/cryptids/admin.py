ea86ca2f3b736d6b78dbe21b44fd8fa27d5ff1a9
Olubukola Olaniyi <bonkeyy2k3@yahoo.com>
2021-03-31 15:06:59 +0100

    Added new sighting

--- cage/cryptids/admin.py
+++ cage/cryptids/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Location, Animal
+from .models import Location, Animal, Sighting
 
 class CityAdmin(admin.ModelAdmin):
     list_display = ('name', 'area')
 
 admin.site.register(Animal)
 admin.site.register(Location)
+admin.site.register(Sighting)
 # Register your models here.
 
